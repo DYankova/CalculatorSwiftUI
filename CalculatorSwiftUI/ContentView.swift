0873a64d28ec40d3a6af35cc9957eb4cf4cf3555
DilyanaYankova <@linkmobility.com>
2020-01-21 17:18:35 +0200

    first commit

---
+++ CalculatorSwiftUI/ContentView.swift
@@ -0,0 +1,134 @@
+//
+//  ContentView.swift
+//  CalculatorSwiftUI
+//
+//  Created by Dilyana Yankova on 18.01.20.
+//  Copyright © 2020 Dilyana Yankova. All rights reserved.
+//
+
+import SwiftUI
+
+    enum CalculatorButton : String {
+        case zero, one, two, three, four, five, six, seven, eight, nine
+        case equals, plus, minus, multiply, divide
+        case ac, plusMinus, percent, decimal
+        
+        var title: String {
+            switch self {
+            case .zero: return "0"
+            case .one: return "1"
+            case .two: return "2"
+            case .three:return "3"
+            case .four:return "4"
+            case .five:return "5"
+            case .six:return "6"
+            case .seven:return "7"
+            case .eight:return "8"
+            case .nine:return "9"
+            case .plus:return "+"
+            case .minus:return "-"
+            case .percent:return "%"
+            case .multiply:return "X"
+            case .divide:return "/"
+            case .equals:return "="
+            case .decimal:return "."
+                
+            default:
+                return "ac"
+            }
+        }
+        
+        var backgroundColor:Color {
+            switch self {
+            case .zero, .one, .two, .three, .four, .five , .six, .seven, .eight , .nine:
+               return Color(.darkGray)
+            case .ac, .plusMinus:
+                return Color(.lightGray)
+            default:
+                return Color(.orange)
+            }
+        }
+    }
+
+//env object
+//treat it as the global application state
+class GlobalEnvironment: ObservableObject {
+    @Published var display = ""
+    
+    func receiveInput(calculatorButton: CalculatorButton){
+        self.display = calculatorButton.title
+    }    
+}
+
+struct ContentView: View {
+    
+    @EnvironmentObject var env : GlobalEnvironment
+    
+    let buttons : [[ CalculatorButton]] = [
+        [.ac, .plus, .minus, .divide],
+        [.one, .two, .three, .plus],
+        [.four, .five, .six, .minus],
+        [.seven, .eight, .nine, .percent],
+        [.zero,  .decimal, .equals]
+    ]
+
+    var body: some View {
+        ZStack(alignment: .bottom) {//to be to the safearea
+            
+            Color.black.edgesIgnoringSafeArea(.all)
+            //two HStack-s
+            VStack (spacing: 12) {
+                HStack (spacing: 12){
+                    Text(env.display).foregroundColor(.white)
+                        .font(.system(size: 72))
+                        .padding()
+                }
+                
+                ForEach(buttons, id : \.self) {row in
+                    HStack {
+                        ForEach(row, id: \.self) { button in
+                           CalculatorButtonView(button: button)
+                        }
+                    }
+                }
+                
+            }.padding(.bottom)
+        }
+    }
+}
+
+struct CalculatorButtonView :View {
+   
+    var button: CalculatorButton
+    
+    //in order not to make bindings, kind of global singleton
+    @EnvironmentObject var env: GlobalEnvironment
+    
+    var body: some View {
+       Button(action: {
+        self.env.receiveInput(calculatorButton: self.button)
+                              
+       }) {
+          Text(button.title)
+            .font(.system(size: 32))
+            .frame(width: self.buttonWidth(button: button), height: (UIScreen.main.bounds.width - 5 * 12) / 4)
+            .foregroundColor(.white)
+            .background(button.backgroundColor)
+            .cornerRadius(self
+            .buttonWidth(button: button))
+      }
+    }
+    
+    private func buttonWidth(button : CalculatorButton) -> CGFloat {
+        if button == .zero {
+             return (UIScreen.main.bounds.width - 4 * 12) / 4 * 2
+        }
+        return (UIScreen.main.bounds.width - 5 * 12) / 4
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environmentObject(GlobalEnvironment())
+    }
+}
